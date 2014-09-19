da01361da8e3f5ada199ad2d2436afdc30f37749
tator <tatortotman@charter.net>
2014-09-19 13:49:59 -0500

    Update fs.java

--- src/fs.java
+++ src/fs.java
@@ -4,14 +4,10 @@ import java.io.FileNotFoundException;
 import java.util.Formatter;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
- *
- * @author 738493
+ *<strong>Author:<\strong> Tator 
+ * 
  */
 public class fs {
 
