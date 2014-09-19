81f003ac79defcda7c947532cb422bc15bb4d896
tator <tatortotman@charter.net>
2014-09-19 13:44:43 -0500

    Update fd.java

--- src/fd.java
+++ src/fd.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *<strong>Author:</strong> Tator & SeaSee
+ *<strong>Author:</strong> Tator
  * <p>
  * <strong>Version:</strong> win.1.1
  */
